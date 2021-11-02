5442f6fb6f0147ce641a23935e010861698d8b20
Hash52 <hirokinoakaunto@gmail.com>
2021-11-02 23:18:48 +0900

    add: トップページ

---
+++ flashcard_app/resources/views/top.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('title', 'top')
+
+@section('content')
+<div class="container">
+    <div class="d-flex justify-content-center">
+        <a role="button" href="#" class="btn btn-outline-info">Game Play</a>
+    </div>
+</div>
+@endsection
